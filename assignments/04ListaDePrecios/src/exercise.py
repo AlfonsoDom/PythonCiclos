c5b2a3bab8a720a0ecf9141694f20850e0f92197
AlfonsoDom <110926482+AlfonsoDom@users.noreply.github.com>
2022-09-05 12:35:04 -0500

    Update exercise.py

--- assignments/04ListaDePrecios/src/exercise.py
+++ assignments/04ListaDePrecios/src/exercise.py
@@ -1,5 +1,19 @@
 def main():
     #escribe tu código abajo de esta línea
-
+num=0
+for i in range(1000000):
+    n=input()
+    if n=='A':
+        print('120')
+        num=num+120
+    elif n=='B':
+        print('250')
+        num=num+250
+    elif n=='C':
+        print('360')
+        num=num+360
+    elif n=='X':
+        print(num)
+        break
 if __name__=='__main__':
     main()
