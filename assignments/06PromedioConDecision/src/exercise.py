252ebecb1ab4718ed2379de3013943e0ccd22608
AlfonsoDom <110926482+AlfonsoDom@users.noreply.github.com>
2022-09-05 12:24:47 -0500

    Update exercise.py

--- assignments/06PromedioConDecision/src/exercise.py
+++ assignments/06PromedioConDecision/src/exercise.py
@@ -1,6 +1,16 @@
 def main():
     #escribe tu código abajo de esta línea
+tot=0
+prom=0
+for i in range(5000):
+    promedio=float(input())
+    if promedio>=0:
+        prom= 1+prom
+        tot= promedio+tot
     
+    if promedio<0:
+        print(tot/prom)
+        break
 
 if __name__=='__main__':
     main()
